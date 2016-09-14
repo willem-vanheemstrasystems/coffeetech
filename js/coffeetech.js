628408b9d608fedbaf81d1427b5fd262a70e3b83
Willem van Heemstra <willem@vanheemstrasystems.com>
2016-09-14 01:18:05 +0100

    Update coffeetech.js

--- js/coffeetech.js
+++ js/coffeetech.js
@@ -3,7 +3,7 @@
 var mod = angular.module( 'coffeetech', []);
 
 mod.controller( 'GithubCtrl', function( $scope ) {
-  var github = new GitHub({});
+  //var github = new GitHub({});
   var github = new GitHub({ token:'MY_OAUTH_TOKEN' });
   var repo = github.getRepo( "gollum", "gollum" );
   repo.show(function(err, repo) {
