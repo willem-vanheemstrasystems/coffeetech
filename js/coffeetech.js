7e534389ec90a829baae1779a96cda2d7e1c5989
Willem van Heemstra <willem@vanheemstrasystems.com>
2016-09-14 21:41:16 +0100

    Update coffeetech.js

--- js/coffeetech.js
+++ js/coffeetech.js
@@ -9,8 +9,8 @@ mod.controller( 'GithubCtrl', function( $scope ) {
     token: 'MY_OAUTH_TOKEN'
   });
 
-  var yahoo = gh.getOrganization('yahoo');
-    yahoo.getRepos(function(err, repos) {
+  var gollum = gh.getOrganization('gollum');
+    gollum.getRepos(function(err, repos) {
      // look at all the repos!
      $scope.repo = repos;
      $scope.$apply();
